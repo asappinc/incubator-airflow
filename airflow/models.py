c768d5c4ebcabe1fe7a910fcba3be7df7bf6f386
bo <bo.blanton@gmail.com>
2018-08-10 21:43:17 -0700

    in the case of no end date return a resonable value
    if the Pod “fails” and we set thigns to “up for retry” (based on the previous commit), the task instance has not “ended” properly, soe we need to make best guess retry time

--- airflow/models.py
+++ airflow/models.py
@@ -1401,6 +1401,9 @@ class TaskInstance(Base, LoggingMixin):
             delay = timedelta(seconds=delay_backoff_in_seconds)
             if self.task.max_retry_delay:
                 delay = min(self.task.max_retry_delay, delay)
+
+        if self.end_date is None:
+            return datetime.now() + delay
         return self.end_date + delay
 
     def ready_for_retry(self):
