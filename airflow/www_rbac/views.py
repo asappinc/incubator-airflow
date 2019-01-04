ec86c1ddfcc5a51f80f1a44053767845812c7fd3
bo <bo.blanton@gmail.com>
2019-01-04 15:42:03 -0800

    safe

--- airflow/www_rbac/views.py
+++ airflow/www_rbac/views.py
@@ -379,18 +379,20 @@ class Airflow(AirflowBaseView):
         data = {}
         for dag_id, state, count in qry:
             if 'all_dags' in filter_dag_ids or dag_id in filter_dag_ids:
+                safe_dag_id = models.DAG.to_safe_dag_id(dag_id)
                 if dag_id not in data:
-                    data[dag_id] = {}
-                data[dag_id][state] = count
+                    data[safe_dag_id] = {}
+                data[safe_dag_id][state] = count
         session.commit()
 
         if 'all_dags' in filter_dag_ids:
             filter_dag_ids = [dag_id for dag_id, in session.query(models.DagModel.dag_id)]
         for dag_id in filter_dag_ids:
-            payload[dag_id] = []
+            safe_dag_id = models.DAG.to_safe_dag_id(dag_id)
+            payload[safe_dag_id] = []
             for state in State.task_states:
                 count = data.get(dag_id, {}).get(state, 0)
-                payload[dag_id].append({
+                payload[safe_dag_id].append({
                     'state': state,
                     'count': count,
                     'dag_id': dag_id,
