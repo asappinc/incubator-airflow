466bd0efa9b345d1ad5f2fa422564abd3ea4c21e
bo <bo.blanton@gmail.com>
2021-06-09 11:22:56 -0600

    force timedelta

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -928,8 +928,10 @@ class TaskInstance(Base, LoggingMixin):  # pylint: disable=R0902,R0904
             delay_backoff_in_seconds = min(modded_hash, timedelta.max.total_seconds() - 1)
             delay = timedelta(seconds=delay_backoff_in_seconds)
             if self.task.max_retry_delay:
-                print(self.task.max_retry_delay, delay, self.task.task_id, self.task.dag_id)
-                delay = min(self.task.max_retry_delay, delay)
+                td = self.task.max_retry_delay
+                if isinstance(td, int) or isinstance(td, float):
+                    td = timedelta(seconds=td)
+                delay = min(td, delay)
         return self.end_date + delay
 
     def ready_for_retry(self):
