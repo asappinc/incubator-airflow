b4189cd01068be292ea95c6166d83780de066cd4
bo <bo.blanton@gmail.com>
2021-07-17 19:22:41 -0600

    revert this PR https://github.com/apache/airflow/pull/15172
    i know it’s not “exact” but setting tasks to FAILED when there are more reties is soooo “upsetting” to a lot of tasks and dags that it’s not worth it.

--- airflow/jobs/local_task_job.py
+++ airflow/jobs/local_task_job.py
@@ -78,15 +78,15 @@ class LocalTaskJob(BaseJob):
             """Setting kill signal handler"""
             self.log.error("Received SIGTERM. Terminating subprocesses")
             self.on_kill()
-            self.task_instance.refresh_from_db()
-            # TODO: [bobo] this behavior (the original is FAILED) really SUCKS as it causing things
+            # TODO: [bobo] this behavior really SUCKS as it causing things
             # to never retry when they should .. sigterms can come from
             # all sorts of places "outside" airflow in K8 and this behavior halts
             # all the jobs .. if something is truly wrong then after retries this will
             # eventually fail
-            if self.task_instance.state not in State.finished:
-                self.task_instance.set_state(State.UP_FOR_RETRY)
-            self.task_instance._run_finished_callback(error="task received sigterm")
+            # self.task_instance.refresh_from_db()
+            # if self.task_instance.state not in State.finished:
+            #    self.task_instance.set_state(State.FAILED)
+            # self.task_instance._run_finished_callback(error="task received sigterm")
             raise AirflowException("LocalTaskJob received SIGTERM signal")
 
         signal.signal(signal.SIGTERM, signal_handler)
